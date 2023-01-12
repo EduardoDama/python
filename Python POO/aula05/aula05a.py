1034f24403ab7f0a59631d6e65413b02d388b724
EduardoDama <102323022+EduardoDama@users.noreply.github.com>
2023-01-11 22:53:22 -0300

    atualizado

---
+++ Python POO/aula05/aula05a.py
@@ -0,0 +1,18 @@
+class issoAi:
+    def __init__(self):
+        self.publico = 'Isso é público!'
+        self._protected = 'isso é protegido'
+        self.__privet = ' Isso é privado'
+
+    def publico(self):
+        return 'Método publico'
+    
+    def _protegido(self):
+        return 'Método protegido'
+
+    def __privado(self):
+        return 'Método privado'
+
+
+f = issoAi()
+
