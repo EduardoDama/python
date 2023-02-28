e2394e05672c698957c98732b0977210cf54e3c8
EduardoDama <102323022+EduardoDama@users.noreply.github.com>
2023-02-28 19:20:25 -0300

    atualização

--- Python POO/exercicios/banco/main.py
+++ Python POO/exercicios/banco/main.py
@@ -1,30 +1,100 @@
 from abc import ABC, abstractmethod
 
 class conta(ABC):
+    def __init__(self, num, agen, sald):
+        self.num = num
+        self.agen = agen
+        self.sald = sald
+
     @abstractmethod
     def sacar():
         ...
 
-class contaPoupança(conta):
-    def sacar():
-        print('Sacando...')
+    @abstractmethod
+    def depositar():
+        ...
 
-    def deposito():
-        print('despositando...')
-        
+class contaPoupança(conta):
+    def __init_subclass__(cls) -> None:
+        return super().__init_subclass__()
+    def sacar(self):
+        print('Sacando...')
+        valor = int(input('Qual valor a ser retirado? '))
+        if valor > 1000:
+            print('Não se pode tirar um valor acima de 1000$')
+        else:
+            self.sald -= valor
+            print('sacando...')
+            print(f'Valor no banco é {self.sald}')
+
+    def depositar(self):
+        valor = int(input('Qual seria o valor a ser depositado? '))
+
+        if valor > 2000:
+            print('Não se pode colocar um valor acima de 2000R$')
+        else:
+            self.sald += valor
+            print('despositando...')
+            print(f'Valor no banco é {self.sald}')
 
 class contaCorrente(conta):
-    def sacar():
+    def __init_subclass__(cls) -> None:
+        return super().__init_subclass__()
+    
+    def sacar(self):
         print('Sacando...')
+        valor = int(input('Qual valor a ser retirado? '))
+        if valor > 2500:
+            print('Não se pode tirar um valor acima de 2500$')
+        else:
+            self.sald -= valor
+            print('sacando...')
+            print(f'Valor no banco é {self.sald}')
 
-    def deposito():
-        print('despositando...')
+    def depositar(self):
+        valor = int(input('Qual seria o valor a ser depositado? '))
+
+        if valor > 8000:
+            print('Não se pode colocar um valor acima de 8000$')
+        else:
+            self.sald += valor
+            print('despositando...')
+            print(f'Valor no banco é {self.sald}')
 
 
 
 
 class pessoa:
-    ...
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    @property
+    def name(self):
+        return self.nome
+    
+    @property
+    def years(self):
+        return self.idade
+    
 
 class Cliente(pessoa):
-    ...
+    def __init__(self, nome, idade, cont):
+        super().__init__(nome, idade)
+        self.conta = cont
+        
+    
+class banco:
+    def __init__(self, cliente, conta):
+        self.cliente = cliente
+        self.conto = conta
+
+conC = contaCorrente(123, 809, 0)
+conP = contaPoupança(123, 809, 0)
+
+conP.depositar()
+
+conP.sacar()
+p1 = Cliente('Eduardo', 15, conP)
+
+
